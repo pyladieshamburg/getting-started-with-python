71434aea604a6e381fa92627ea5e0c48f03a3372
Tereza Iofciu <tereza@macbook>
2023-07-18 12:38:15 +0200

    add setup with just

---
+++ setup-with-just/setup-just-and-terminal.sh
@@ -0,0 +1,11 @@
+#install just
+echo -e "BREWING JUST"
+brew install just
+
+#configuring your terminal part 1
+echo "CONFIGURING YOUR TERMINAL"
+just setup-terminal-part1
+
+#activate the changes of the first step
+echo "ACTIVATING ZSHELL"
+source ~/.zshrc
